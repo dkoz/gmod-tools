64eb54bb84dd522a8a977944ee2db8bc90058db1
dkoz <drunkenkoz@gmail.com>
2022-08-18 09:33:57 -0400

    Just a better example for vip jobs

--- example-vipjobs.lua
+++ example-vipjobs.lua
@@ -1,3 +1,21 @@
+jobz = {}
+
+-- Table for allowed ranks.
+jobz.allowedRanks = {
+	"superadmin",
+	"vip"
+}
+
+-- Do not edit this code
+function VIPGroup(ply)
+	local rank = jobz.allowedRanks
+	return CLIENT or table.HasValue(rank,ply:GetNWString("usergroup"))
+end
+
+--[[
+	Example jobs for the scripts.
+	This allows for easy vip jobs.
+--]]
 TEAM_EXAMPLE = DarkRP.createJob("Example Job", {
     color = Color(255, 255, 255),
     model = "models/player/hostage/hostage_01.mdl",
@@ -8,6 +26,20 @@ TEAM_EXAMPLE = DarkRP.createJob("Example Job", {
     salary = 50,
     admin = 0,
     vote = false,
-    customCheck = function(ply) return ply:GetUserGroup() == "vip" end,
+    customCheck = function(ply) return VIPGroup(ply) end,
+    CustomCheckFailMsg = "You need to be VIP for this job.",
+})
+
+TEAM_EXAMPLE2 = DarkRP.createJob("Example Job 2", {
+    color = Color(255, 255, 255),
+    model = "models/player/hostage/hostage_01.mdl",
+    description = [[]],
+    weapons = {},
+    command = "example2",
+    max = 3,
+    salary = 50,
+    admin = 0,
+    vote = false,
+    customCheck = function(ply) return VIPGroup(ply) end,
     CustomCheckFailMsg = "You need to be VIP for this job.",
 })
